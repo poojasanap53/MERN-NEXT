16768ce18f02e6ebf63d0a1fe015057791423e02
Pooja Sanap <sanappooja777@gmail.com>
2024-03-16 14:24:32 +0530

    made some minor changes

--- brainwave/src/components/Benefits.jsx
+++ brainwave/src/components/Benefits.jsx
@@ -13,7 +13,7 @@ const Benefits = () => {
           className="md:max-w-md lg:max-w-2xl "
           title="Chat Smarter, Not Harder with Brainwave"
         />
-        <div className="flex flex-wrap gap-10 mb-10">
+        <div className="flex flex-wrap gap-10 mb-10 justify-center items-center">
           {benefits.map((item) => (
             <div
               className="block relative p-0.25 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] "
