f227eaed362b8481fcacf0f45a2b83272b82a36e
Pooja Sanap <sanappooja777@gmail.com>
2024-03-16 14:22:38 +0530

    added routers

--- brainwave/src/main.jsx
+++ brainwave/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
-
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
